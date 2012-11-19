c734372c4274502f3d0149bc33bbf5b553c21493
Joel Pelton <joel@joel-HP-Compaq-4000-Pro-SFF-PC.(none)>
2012-11-19 15:25:52 -0600

    test

---
+++ bing.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python 
+
+import webbrowser 
+import time
+
+i = 0
+# create 45 tabs..
+while i < 45:
+	i += 1
+	url = 'http://www.bing.com/search?q=%d' % (i,)
+	#webbrowser.open_new_tab(url)
+	print url
+
+
+# then close the browser after they have had time to load
+end = time.time() + 10
+while 1:
+	if time.time() > end:
+		print "End"
+		break
+
